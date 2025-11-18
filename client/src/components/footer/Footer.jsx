ee43f740837bea1d8f7c22bfbe128fe586be1dcb
Maya Ilieva <ilieva1maya@gmail.com>
2025-11-18 11:27:22 +0200

    FontAwesome icons

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -5,13 +5,12 @@ export default function Footer() {
         <footer id="footer">
             <div className="inner">
                 <ul className="icons">
-                    <li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
-                    <li><a href="#" className="icon brands fa-github"><span className="label">Github</span></a></li>
-                    <li><a href="#" className="icon brands fa-dribbble"><span className="label">Dribbble</span></a></li>
-                    <li><a href="#" className="icon solid fa-envelope"><span className="label">Email</span></a></li>
+                    <li><a href="#" className="icon fa-brands fa-github"></a></li>
+                    <li><a href="#" className="icon fa-brands fa-linkedin"></a></li>
+                    <li><a href="#" className="icon fa-solid fa-envelope"></a></li>
                 </ul>
                 <ul className="copyright">
-                    <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                    <li>&copy; <a href="https://github.com/ilieva1maya">Maya Ilieva</a></li>
                 </ul>
             </div>
         </footer>
